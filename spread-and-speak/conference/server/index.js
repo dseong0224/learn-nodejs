50c82e7bde9095471ab384526f419e81d1bf9cba
Daniel Seong <dseong0224@gmail.com>
2019-11-06 23:42:13 -0800

    defined default behaviour for empty favicon

--- spread-and-speak/conference/server/index.js
+++ spread-and-speak/conference/server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 
-const routes = require('./routes')
-
-app.use('/',routes())
+const routes = require('./routes');
+app.use(express.static('public'));
+app.get('/favicon.ico', (req, res, next) => {
+  return res.sendStatus(204);
+})
+app.use('/', routes());
 
 app.listen(3000);
 
