40169d88133edf8b447acbfaf7f27d7df0f3bae1
Daniel Seong <dseong0224@gmail.com>
2019-11-12 20:14:01 -0800

    dynamically pull pull feedback from database

--- spread-and-speak/conference/server/routes/feedback/index.js
+++ spread-and-speak/conference/server/routes/feedback/index.js
@@ -2,9 +2,20 @@ const express = require('express');
 
 const router = express.Router();
 
-module.exports = () => {
-  router.get('/', (req, res, next) => {
-    return res.send('Feedback');
+module.exports = (param) => {
+
+  const { feedbackService } = param;
+
+  router.get('/', async (req, res, next) => {
+    try {
+      const feedbacklist = await feedbackService.getList();
+      return res.render('feedback', {
+        page: 'Feedback',
+        feedbacklist,
+      });
+    } catch (err) {
+      return err;
+    }
   });
   router.post('/', (req, res, next) => {
     return res.send('Form sent');
